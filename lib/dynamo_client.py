3047ada44a15afe09a9d934d7ab97b42f9307b52
Justin Harwood <justin@harwoodjp.com>
2023-01-03 17:19:43 -0600

    readd lib/

---
+++ lib/dynamo_client.py
@@ -0,0 +1,9 @@
+import boto3
+
+
+class DynamoClient:
+    def __init__(self, table: str):
+        dynamo = boto3.resource("dynamodb")
+        self.table = dynamo.Table(table)
+    def put_item(self, item: dict):
+        self.table.put_item(Item=item)
