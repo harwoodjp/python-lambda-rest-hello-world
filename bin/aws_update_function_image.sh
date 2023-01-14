7c4caac9422a01877185ec49dc4fa0909ba8b8f4
Justin Harwood <justin@harwoodjp.com>
2023-01-14 16:48:19 -0600

    aws_update_function_image

--- bin/aws_update_function_image.sh
+++ bin/aws_update_function_image.sh
@@ -1,4 +1,4 @@
 # $1: AWS FUNCTION NAME
 aws lambda update-function-code \
-    --function-name $AWS_FUNCTION_NAME \
+    --function-name $1 \
     --image-uri $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$1:latest
